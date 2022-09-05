91a58e10c58e8848a27cc4d7c3b5c37763e1b3b7
irwin <irwinlkl@gmail.com>
2022-09-06 04:57:00 +0800

    added player class

--- gui.py
+++ gui.py
@@ -3,6 +3,7 @@ import sys, pygame
 pygame.init()
 
 width = 1000
+
 height = 600
 ## convert_alpha()## convert image file to help pygame load faster
 #setting screen size
@@ -11,21 +12,42 @@ screen = pygame.display.set_mode((1000,600))
 #setting frame rate cap tp run stabily
 clock = pygame.time.Clock()
 
-
+bgx = 750
 #loading image to surface
-test_surface = pygame.image.load("rpgsprites1/bg/bg.png").convert_alpha()
-test_surf = pygame.transform.scale(test_surface,(width,height) )
+first_bg_surface = pygame.image.load("assets/bgg.jpeg").convert_alpha()
+first_bg_surf = pygame.transform.scale(first_bg_surface,(1000,600) )
 #get the rectangle of the surface to further control ht position
-test_surface_rect = test_surf.get_rect(topleft =(0,0))
+first_bg_surface_rect = first_bg_surf.get_rect(topleft =(0,0))
+
+second_bg_surface = pygame.image.load("assets/bgg.jpeg").convert_alpha()
+second_bg_surf = pygame.transform.scale(second_bg_surface,(1000,600) )
+#get the rectangle of the surface to further control ht position
+second_bg_surface_rect = second_bg_surf.get_rect(topleft =(bgx,0))
 
 #font
 test_font = pygame.font.Font(None,50)
-text_surface = test_font.render("welcome",False,"white")
+text_surface = test_font.render("Space Impact Remake",False,"white")
 
-
-
+x = 0
+twox = 1000
 #setting screen title
-pygame.display.set_caption("assignment")
+pygame.display.set_caption("Space Impact Remake")
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("assets/player.png")
+        self.resize =  pygame.transform.scale(self.image,(200,120))
+        self.rect = self.resize.get_rect()
+        self.centerx = 100
+        self.centery = 300
+        self.speedx = 0
+        
+    def update(self):
+        self.rect.x += self.speedx
+all_sprite = pygame.sprite.Group()
+player = Player()
+all_sprite.add(player)
 while True:
     # for every event check that if user click on cross of the screen
     #then quit the game
@@ -34,9 +56,21 @@ while True:
         if event.type == pygame.QUIT:
             pygame.QUIT()
             exit()
+    all_sprite.update()
+    #scrolling back ground 
+    x -= 2
+    twox -= 2
+    if x <= -1000:
+        x = 0
+    if twox <=0:
+        twox = 1000
     
-    screen.blit(test_surf,test_surface_rect)
-    screen.blit(text_surface,(400,100))
+    screen.blit(first_bg_surf,(x,0))
+    screen.blit(second_bg_surf,(twox,0))
+    screen.blit(text_surface,(300,100))
+    all_sprite.draw(player.image)
+    
+    
 
     # constanly update gameboard        
     pygame.display.update()
