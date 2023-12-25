2be741f57a18138ea1bc1da986e7b71ed3a0b17b
irwin <irwinlkl@gmail.com>
2023-12-26 07:18:42 +0800

    a

--- sprite.py
+++ sprite.py
@@ -2,9 +2,12 @@ import sys, pygame
 from typing import Any
 
 
+collide = 0
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
+        self.alive = True
+        self.cooldown = 0
         self.image = pygame.image.load("assets/player1.png")
         self.resize =  pygame.transform.scale(self.image,(100,50))
         self.rect = self.resize.get_rect()
@@ -36,11 +39,34 @@ class Player(pygame.sprite.Sprite):
             self.speedy = + 4 
         self.rect.x += self.speedx
         self.rect.y += self.speedy    
-        if self.cooldown == 0:
-            self.cooldown = 20  
-        
+        #player boundaries
+        if self.rect.x < 0:
+            self.rect.x = 0
+        if self.rect.x > 900:
+            self.rect.x = 900
+        if self.rect.y > 550:
+            self.rect.y = 550
+        if self.rect.y < 0:
+            self.rect.y = 0
 
-        
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.alive = True
+        self.cooldown = 0
+        self.image = pygame.image.load("assets/enemy.png")
+        self.resize =  pygame.transform.scale(self.image,(1,1))
+        self.rect = self.resize.get_rect()
+        self.rect.centerx = 1100
+        self.rect.centery = random.randint(200,600)
+      
+    def update(self):
+        self.rect.x -= 1
+        #if pygame.sprite.spritecollide(Enemy,Bullets,False):
+        #    self.kill()
+        if self.rect.x < 0:
+            self.kill()
         
 
         
@@ -54,10 +80,11 @@ class Bullets(pygame.sprite.Sprite):
         self.resize = pygame.transform.scale(self.image,(5,5))
         self.w = self.image.get_width()
         self.h = self.image.get_height()
-        self.resize = pygame.transform.scale(self.image,(self.w*0.2,self.h*0.2))
         self.rect = self.resize.get_rect()
         self.rect.center = (x,y)
-        
+    
+    
+
     def update(self):
         self.rect.centerx += 10
         if self.rect.x > 1200:
